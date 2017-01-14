97d3840171ba9191a46e1f459bcd80fc2e3b916f
Jiyuan Liu <liujiyuan126@yeah.net>
2017-01-14 11:10:43 +0800

    Add some notices + 在输出里增加了一些说明 + 在 README 里增加了一些使用方法的说明

--- README.md
+++ README.md
@@ -19,12 +19,26 @@
     * BeautifulSoup 4
 
 ### 命令行版本
+使用前请先确认安装了 Python 2.7 版本，并安装了相应的库。  
+Python 2.7 版本请自行去官网下载，官网网址请百度。  
+安装必要的 Python 库建议使用 pip，在 shell 下运行下面的命令：
+
+```bash
+pip install beautifulsoup4
+```
+
 Linux 系统下使用任意 shell 运行下面的命令。  
 Windows 系统下推荐使用 Git Shell 运行下面的命令。
 
 ```bash
 git clone https://github.com/MrWhoami/whoamibangumi.git
 python main.py
+```
+
+如果想要输出到文件，请使用系统自带的重定向，同样是 shell 命令，文件名并不一定是 `OUTPUT.txt`：
+
+```
+python main.py > OUTPUT.txt
 ```
 
 ### 网页版本
--- main.py
+++ main.py
@@ -5,7 +5,17 @@ from Youku import getYouku
 from Iqiyi import getIqiyi
 from PPTV import getPPTV
 from AcFun import getAcfun
+from datetime import date
 import sys
+
+def printNotice():
+    """Print notice info"""
+    print u"""
+注意 Notice：
+    1. Bilibili 的番组在今天之前的两天显示的是正常的已经更新的内容，而今天之后标记的更新信息均为将要更新的内容，而不是已经更新的内容，今天则标记的是今天将要更新的内容，也可能是已经更新的，如：今天是周三，则周一、周二显示的是更新之后的内容，周四、周五、周六、周日显示的是即将更新的内容；
+    2. 不显示更新信息的番剧可能是大长篇，也有可能是平台自制的节目；
+    3. 今天是 {0}
+    """.format(date.today().strftime('%A'))
 
 # For Chinese printing orz
 type = sys.getfilesystemencoding()
@@ -26,4 +36,4 @@ errorCount += getAcfun().cmdPrint()
 
 # End statistics
 print u'\n出错的网站数量：{}'.format(errorCount).encode(type)
-print ''
+printNotice()
